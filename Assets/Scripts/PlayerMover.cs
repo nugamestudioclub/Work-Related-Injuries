6d372c70d13e84e661ae092d8ce245f5ce241156
Christianfitz22 <51420782+Christianfitz22@users.noreply.github.com>
2024-02-16 19:56:56 -0500

    Basic wall collision detection for player movement
    Updated PlayerMover to not be able to move into spaces occupied by colliders marked with the Wall LayerMask.
    In MovementTest, a tilemap object called Tilemap_Walls has tile colliders and is marked with the Wall layermask to interact with this.

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -12,6 +12,9 @@ public class PlayerMover : MonoBehaviour
     // the amount of distance between centers of adjacent tiles in world space
     [SerializeField] private float tileSize;
 
+    [Header("LayerMasks")]
+    [SerializeField] private LayerMask wallLayer;
+
     [Header("Player Properties")]
     [SerializeField] private float walkSpeed;
     [SerializeField] private float sprintSpeed;
@@ -45,26 +48,40 @@ public class PlayerMover : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, targetTile, moveSpeed * Time.deltaTime);
 
         if (transform.position == targetTile) {
+            // select next tile mechanic
             if (Mathf.Abs(yInput) == 1f) {
-                targetTile += new Vector3(0f, tileSize * yInput, 0f);
+                attemptMoveTargetBy(new Vector3(0f, tileSize * yInput, 0f));
             } else if (Mathf.Abs(xInput) == 1f) {
-                targetTile += new Vector3(tileSize * xInput, 0f, 0f);
+                attemptMoveTargetBy(new Vector3(tileSize * xInput, 0f, 0f));
             }
         } else {
+            // backup mechanic
             Vector3 displacement = targetTile - transform.position;
             if (Mathf.Abs(yInput) == 1f) {
                 if (transform.position.x == targetTile.x) {
                     if (displacement.y * yInput < 0) {
-                        targetTile += new Vector3(0f, tileSize * yInput, 0f);
+                        attemptMoveTargetBy(new Vector3(0f, tileSize * yInput, 0f));
                     }
                 }
             } else if (Mathf.Abs(xInput) == 1f) {
                 if (transform.position.y == targetTile.y) {
                     if (displacement.x * xInput < 0) {
-                        targetTile += new Vector3(tileSize * xInput, 0f, 0f);
+                        attemptMoveTargetBy(new Vector3(tileSize * xInput, 0f, 0f));
                     }
                 }
             }
         }
+    }
+
+    private void attemptMoveTargetBy(Vector3 relativeMove) {
+        if (targetTileOpen(targetTile + relativeMove)) {
+            targetTile += relativeMove;
+        } else {
+            Debug.Log("Move failed");
+        }
+    }
+
+    private bool targetTileOpen(Vector3 targetPos) {
+        return !Physics2D.OverlapBox(targetPos, new Vector2(0.8f, 0.8f), 0f, wallLayer);
     }
 }
